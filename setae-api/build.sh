b549de3eb89811ac99b018fcf01196b3026bb07b
Katherine Bargar <kbargar@fivecolleges.edu>
2025-10-23 16:19:05 +0000

    Add a file to build and test the container

---
+++ setae-api/build.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+pushd ./setae-api >/dev/null || exit
+old_version="$(git rev-parse --short=8 HEAD)"
+git fetch
+git pull
+version="$(git rev-parse --short=8 HEAD)"
+popd >/dev/null || exit
+if [[ $old_version != "$version" ]]; then
+	echo "have $version (was $old_version)"
+else
+	echo "have $version (no changes)"
+fi
+
+if [[ ${1:-} == "--relock" ]]; then
+	uv pip compile requirements.txt -o requirements.lock
+	git diff requirements.txt
+fi
+
+build="$RANDOM"
+echo "building $build"
+docker build \
+	-t edu.fivecolleges.libraries.setae-api:latest \
+	-t edu.fivecolleges.libraries.setae-api:"$version" \
+	-t edu.fivecolleges.libraries.setae-api:"$build" \
+	.
+
+setae="$(docker run -d -p "$build":80 edu.fivecolleges.libraries.setae-api:"$build")"
+trap 'docker container rm --force "$setae" >/dev/null' exit
+
+echo "waiting for startup"
+sleep 15
+curl localhost:"$build"
