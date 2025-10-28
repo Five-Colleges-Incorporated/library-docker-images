5f2281041db6ec44ab296edf37652701fd44475b
Katherine Bargar <kbargar@fivecolleges.edu>
2025-10-28 17:13:58 +0000

    Update IP ranges

--- setae-api/do_main.py
+++ setae-api/do_main.py
@@ -6,10 +6,25 @@ from fastapi.responses import Response
 from main import app, read_item
 
 allow_list = [
-    ipaddress.ip_network('150.195.159.183/32'), # Katherine's home
-    ipaddress.ip_network('64.224.254.198/32'), # Aaron's home
+    # https://networksdb.io/ip-addresses-of/amherst-college
+    ipaddress.ip_network('148.85.0.0/16'),
 
-    ipaddress.ip_network('148.85.254.0/23'), # Amherst and FCI
+    # https://www.hampshire.edu/it/user-support/network-wifi-and-vpn/technical-description-our-network
+    # cross-referenced with LDLite ranges
+    ipaddress.ip_network('144.121.36.224/27'),
+    ipaddress.ip_network('64.254.160.0/20'),
+
+    # https://networksdb.io/ip-addresses-of/mount-holyoke-college
+    ipaddress.ip_network('138.110.0.0/16'),
+
+    # https://networksdb.io/ip-addresses-of/smith-college
+    # cross-referenced with LDLite ranges
+    ipaddress.ip_network('131.229.0.0/17'),
+
+    # https://networksdb.io/ip-addresses-of/university-of-massachusetts-amherst
+    # cross-referenced with LDLite ranges
+    ipaddress.ip_network('128.119.0.0/16'),
+    ipaddress.ip_network('205.172.168.0/22'),
 ]
 
 SOURCE_IP_HEADER = 'do-connecting-ip'
