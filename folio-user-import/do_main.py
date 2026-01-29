761abc7ff3b9c95fe56bf8d97bc09064817872a2
Katherine Bargar <kbargar@fivecolleges.edu>
2026-01-29 17:23:15 +0000

    Fix Ruff issues with user import do_main

--- folio-user-import/do_main.py
+++ folio-user-import/do_main.py
@@ -1,3 +1,5 @@
+"""Entrypoint for nightly user import."""
+
 import asyncio
 import logging
 import os
@@ -9,26 +11,27 @@ from zoneinfo import ZoneInfo
 
 import boto3
 from botocore.client import Config
-
-from folioclient import FolioClient
 from folio_data_import.UserImport import UserImporter
+from folioclient import FolioClient
 
 PWD = Path.cwd()
 USERS_FILE = PWD / "users.ndjson"
-ERRORS_FILE = PWD / f"errors"
-LOG_FILE = PWD / f"log"
+ERRORS_FILE = PWD / "errors"
+LOG_FILE = PWD / "log"
 
 logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
 logger = logging.getLogger()
 logger.addHandler(logging.StreamHandler())
 
+
 async def import_users():
+    """Imports users via the folio_data_import library."""
     library_name = os.getenv("LIBRARY_NAME")
     library_code = os.getenv("LIBRARY_CODE")
-    logger.info(f"Replacing {library_name} with {library_code}")
+    logger.info("Replacing %s with %s", library_name, library_code)
     with FileInput(USERS_FILE, inplace=True) as file:
         for line in file:
-            print(line.replace(library_name, library_code), end='')
+            print(line.replace(library_name, library_code), end="")  # noqa: T201
 
     logger.info("Setting up Importer")
     importer = UserImporter(
@@ -40,7 +43,7 @@ async def import_users():
         ),
         library_name=os.getenv("LIBRARY_NAME"),
         batch_size=os.getenv("BATCH_SIZE"),
-        only_update_present_fields=os.getenv("ONLY_UPDATE_PRESENT_FIELDS", 1) == 1,
+        only_update_present_fields=os.getenv("ONLY_UPDATE_PRESENT_FIELDS", "1") == "1",
         user_file_path=USERS_FILE,
         limit_simultaneous_requests=asyncio.Semaphore(10),
         no_progress=True,
@@ -53,23 +56,29 @@ async def import_users():
 @contextmanager
 def _create_spaces_client(key, secret):
     session = boto3.session.Session()
-    with closing(session.client(
-        's3',
-        region_name=os.getenv('SPACES_REGION', 'nyc3'),
-        endpoint_url=os.getenv('SPACES_ENDPOINT', 'https://nyc3.digitaloceanspaces.com'),
-        aws_access_key_id=key,
-        aws_secret_access_key=secret,
-        config=Config(s3={'addressing_style': 'virtual'})
-        )) as s3:
+    with closing(
+        session.client(
+            "s3",
+            region_name=os.getenv("SPACES_REGION", "nyc3"),
+            endpoint_url=os.getenv(
+                "SPACES_ENDPOINT",
+                "https://nyc3.digitaloceanspaces.com",
+            ),
+            aws_access_key_id=key,
+            aws_secret_access_key=secret,
+            config=Config(s3={"addressing_style": "virtual"}),
+        ),
+    ) as s3:
         yield s3
 
 
 def download_users():
-    bucket = os.getenv('USERS_BUCKET')
+    """Dowloads the latest uploaded users file to a fixed location."""
+    bucket = os.getenv("USERS_BUCKET")
 
-    logger.info(f"Finding latest users file in: {bucket}")
+    logger.info("Finding latest users file in: %s", bucket)
     users_key = None
-    with _create_spaces_client(os.getenv('USERS_KEY'), os.getenv('USERS_SECRET')) as s3:
+    with _create_spaces_client(os.getenv("USERS_KEY"), os.getenv("USERS_SECRET")) as s3:
         paginator = s3.get_paginator("list_objects_v2")
         latest = None
         for page in paginator.paginate(Bucket=bucket):
@@ -80,33 +89,38 @@ def download_users():
                     latest = obj
         users_key = latest["Key"] if latest else None
 
-    logger.info(f"downloading latest users file: {users_key}")
-    with open(USERS_FILE, "wb+") as users_file:
+    logger.info("downloading latest users file: %s", users_key)
+    with USERS_FILE.open("wb+") as users_file:
         s3.download_fileobj(bucket, users_key, users_file)
 
+
 def upload_logs():
-    users_bucket = os.getenv('USERS_BUCKET')
-    logs_bucket = os.getenv('LOGS_BUCKET')
+    """Uploads the errors and logs after the import."""
+    users_bucket = os.getenv("USERS_BUCKET")
+    logs_bucket = os.getenv("LOGS_BUCKET")
 
-    now = datetime.now(ZoneInfo(os.getenv('TIMEZONE', "America/New_York"))).isoformat()
+    now = datetime.now(ZoneInfo(os.getenv("TIMEZONE", "America/New_York"))).isoformat()
 
-    with _create_spaces_client(os.getenv('USERS_KEY'), os.getenv('USERS_SECRET')) as s3:
+    with _create_spaces_client(os.getenv("USERS_KEY"), os.getenv("USERS_SECRET")) as s3:
         if ERRORS_FILE.exists() and ERRORS_FILE.stat().st_size > 0:
-            logger.info(f"Uploading errord users file to: {users_bucket}")
+            logger.info("Uploading errored users file to: %s", users_bucket)
             s3.upload_file(ERRORS_FILE, users_bucket, f"failed_users_{now}.ndjson")
         else:
-            logger.info(f"No errors file")
+            logger.info("No errors file")
 
-    with _create_spaces_client(os.getenv('LOGS_KEY'), os.getenv('LOGS_SECRET')) as s3:
+    with _create_spaces_client(os.getenv("LOGS_KEY"), os.getenv("LOGS_SECRET")) as s3:
         library = " ".join(os.getenv("LIBRARY_NAME").strip('"').split()[:-1])
-        logger.info(f"Uploading logs file to: {logs_bucket}/{library}_{now}.log")
-        s3.upload_file(LOG_FILE, logs_bucket, f"{library}_{now}.log")
+        log_key = f"{library}_{now}.log"
+        logger.info("Uploading log file to: %s/%s", logs_bucket, log_key)
+        s3.upload_file(LOG_FILE, logs_bucket, log_key)
 
-async def notify_error(ex):
-    # TODO: post ex to slack
-    ...
+
+async def notify_error(_):
+    """Will send a message to Slack with the error."""
+
 
 async def main():
+    """The cli entrypoint to run the full import."""
     try:
         download_users()
         await import_users()
@@ -114,6 +128,7 @@ async def main():
     except Exception as ex:
         await notify_error(ex)
         raise
+
 
 if __name__ == "__main__":
     asyncio.run(main())
